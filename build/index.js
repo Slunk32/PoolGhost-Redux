ec0d514e64c531ac1a0976031ec90dcb25c77347
Russell Salerno <russell.j.salerno@gmail.com>
2016-08-22 16:14:20 -0700

    added some basic react components, express, routes, and bootstrap styling

--- build/index.js
+++ build/index.js
@@ -1,4 +1,24 @@
 'use strict';
 
-console.log('hi');
+require('babel-polyfill');
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactRouter = require('react-router');
+
+var _routes = require('./routes');
+
+var _routes2 = _interopRequireDefault(_routes);
+
+require('./styles/styles.css');
+
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _reactDom.render)(_react2.default.createElement(_reactRouter.Router, { history: _reactRouter.browserHistory, routes: _routes2.default }), document.getElementById('app')); // webpack imports this
 //# sourceMappingURL=/Users/RSalerno/Documents/coding/poolghost-redux/index.js.map
